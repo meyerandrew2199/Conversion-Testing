7e22dff8c7bbee9bdbf8f799d458e3b9d71bb4f6
Andrew M <107826382+meyerandrew2199@users.noreply.github.com>
2023-06-19 23:55:27 -0400

    Add files via upload
    Just testing converstions

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+// Ask user for fave number
+
+Console.WriteLine("Enter your favorite number: ");
+
+// Turn that answer into an int
+
+    /* Attempt 1, saves answer in a variable, implicit converstion fail
+
+int variableName = Console.ReadLine();
+
+    */
+
+    /*Attempt 2, explicit converstion fails
+
+int variableName = (int)value;
+
+    */
+
+    // Attempt 3, Built in method of conversion works
+int faveNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Wow your favorite number is " + faveNumber + ", so is mine!");
+
+
+
+
