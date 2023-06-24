3b8e511c44b92db443e8d0cdde51772ea48ecb23
meyerandrew2199 <107826382+meyerandrew2199@users.noreply.github.com>
2023-06-24 16:57:22 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -26,4 +26,37 @@ Console.WriteLine("Wow your favorite number is " + faveNumber + ", so is mine!")
 
 
 
+/*Here I am practicing using built in methods for mathmatical problems via code acadmey problems */
+
+
+      // Starting variables 
+      int numberOne = 12932;
+      int numberTwo = -2828472;
+
+      // Use built-in methods and save to variable 
+
+double numberOneSqrt = Math.Floor(Math.Sqrt(numberOne));
+
+      // Use built-in methods and save to variable 
+      // number is negative so must convert to absolute value
+
+double numberTwoSqrt = Math.Floor(Math.Sqrt(Math.Abs(numberTwo)));
+      // Print the lowest number
+Console.WriteLine(Math.Min(numberTwoSqrt, numberOneSqrt));
+
+/* other built in methods */
+      
+      
+      double numberOne = 6.5;
+      double numberTwo = 4;
+
+      // Raise numberOne to the numberTwo power
+      Console.WriteLine(Math.Pow(numberOne, numberTwo));
+
+      // Round numberOne up
+      Console.WriteLine(Math.Ceiling(numberOne));
+
+      // Find the larger number between numberOne and numberTwo
+      Console.WriteLine(Math.Max(numberOne, numberTwo));
+
 
